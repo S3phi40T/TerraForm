35c22faf27fa43d07c9bdd348ac17aad3cae60b3
S3phi40T <sephirotdnf@gmail.com>
2023-02-15 16:02:56 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  description = "AWS region"
+  default     = "eu-central-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.nano"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "BartTerraVM"
+}
+
+variable "project" {
+  description = "project name"
+  default     = "TerraForm"
+}
+
+variable "ritm" {
+  description = "ServiceNow RITM"
+  default     = "RITM12345BART"
+}
+
+variable "application_service" {
+  description = "App"
+  default     = "BartApp"
+}
+
+
