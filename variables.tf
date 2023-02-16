7df3ee6f3912d5003ed905be363a76ac052f5dab
S3phi40T <sephirotdnf@gmail.com>
2023-02-16 13:56:03 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,3 +23,12 @@ variable "ritm" {
 variable "application_service" {
   description = "App"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS_ACCESS_KEY_ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS_SECRET_ACCESS_KEY"
+}
+
